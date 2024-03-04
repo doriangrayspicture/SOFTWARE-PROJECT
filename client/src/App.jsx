974eff1ccc229359e72f183e41d2dbba4e567104
Sunetra Ghosh <ghosh.sunetra12@gmail.com>
2024-03-05 00:36:05 +0530

    connected to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from './assets/pages/SignIn';
 import SignUp from './assets/pages/SignUp';
 import About from './assets/pages/About';
 import Profile from './assets/pages/Profile';
+import Header from './assets/components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path='/' element={<Home />} /> {/* Render Home component for the '/' route */}
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
