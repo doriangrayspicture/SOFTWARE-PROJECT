013cc850f6c7c6f46f845b41fefc4e8055fedb7e
Sunetra Ghosh <ghosh.sunetra12@gmail.com>
2024-03-04 21:13:29 +0530

    Create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Home from './assets/pages/Home';
+import SignIn from './assets/pages/SignIn';
+import SignUp from './assets/pages/SignUp';
+import About from './assets/pages/About';
+import Profile from './assets/pages/Profile';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} /> {/* Render Home component for the '/' route */}
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
