ccbcfa788c59d48bff2c597fc6bdd1c90d74b943
Sunetra Ghosh <ghosh.sunetra12@gmail.com>
2024-03-05 02:28:36 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 const app=express();
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO
+)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000!');
   });
--- client/src/assets/pages/About.jsx
+++ client/src/assets/pages/About.jsx
@@ -1,4 +1,19 @@
 import React from "react";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 export default function About(){
     return(
         <div>About</div>
